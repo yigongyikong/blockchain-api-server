418085d96ec5ff11883069cb4924343ddb0e78a6
yiggyikg <yiggyikg@gmail.com>
2023-01-01 16:44:09 +0900

    [001_1][btc][install bitcoinjs-lib & ecpair & bip32]

---
+++ btc/localtest.js
@@ -0,0 +1,22 @@
+const bitcoin = require('bitcoinjs-lib');
+// console.log(bitcoin); // { address, crypto, networks, payments, script, Block, Psbt, opcodes, Transaction, initEccLib }
+
+// console.log(bitcoin.address); // { fromBase58Check, fromBech32, toBase58Check, toBech32, fromOutputScript, toOutputScript }
+
+// console.log(bitcoin.crypto); // { ripemd160, sha1, sha256, hash160, hash256, taggedHash }
+
+// console.log(bitcoin.networks); // { bitcoin, regtest, testnet }
+
+// console.log(bitcoin.payments); // { embed, p2ms, p2pk, p2pkh, p2sh, p2wpkh, p2wsh, p2tr }
+
+// console.log(bitcoin.script); // { OPS, isPushOnly, compile, decompile, toASM, fromASM, toStack, isCanonicalPubKey, isDefinedHashType, isCanonicalScriptSignature, number, signature }
+
+// console.log(bitcoin.Block); // [class Block]
+
+// console.log(bitcoin.Psbt); // [class Psbt]
+
+// console.log(bitcoin.opcodes); // [OP_code list]
+
+// console.log(bitcoin.Transaction); // [class Transaction]
+
+console.log(bitcoin.initEccLib); // [Function: initEccLib]
